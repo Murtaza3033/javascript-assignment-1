ed70b361afd278b08d8c10a81d6e4a0b451c861e
Murtaza ali qazi <murtazahere303@gmail.com>
2023-09-01 17:43:42 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// !!!!!!!!!chaper # 1 !!!!!!!!!
+// ?answer 1
+// alert('hello Welcome')
+
+// ?answer 2
+// alert('Error!please enter a valid password.')
+
+// ?answer 3
+// alert('Welcome to JS Land..\nHappy coding!')
+
+// ?answer 4
+// alert('Welcome to JS Land...')
+// alert('Happy coding!\n▢prevent this page from creating additional dialog')
+
+// ?answer 5
+// let text;
+//   let text2 = prompt("Please enter your message:");
+//   alert(text2) 
+
+
+
+
+// !!!!!!!!!chaper # 2 !!!!!!!!!
+
+// ?answer 1 and 2 
+// var name='Murtaza ali'
+// var name2=' Qazi'
+// console.log( name + name2)
+
+// ?answer 3 
+// alert('hello world')
+
+// ? answer 4
+// alert('Murtaza ali qazi')
+// alert('age :21')
+// alert('certified web and mobile application developer')
+
+// ? answer 5
+// alert('pizza \npizz \npiz \npi \np')
+    
+// // ? answer 6
+// var email='MAQ303@gmail.com'
+// alert('my email address is '+email)
+
+// ? answer7
+// var book='I Am Trying To Learn From Smarter Way To Learn JS'
+// alert(book)
+
+// ? answer 8  
+// document.writeln('my name is Murtaza ali qazi')
